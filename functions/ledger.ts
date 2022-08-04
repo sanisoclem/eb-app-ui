ddbe9811b2b7793e9190d27e50415bc655b2d1cc
Jerahmeel Cosinas <me@jerahmeelcosinas.net>
2022-08-04 23:29:27 +1000

    stuff

--- functions/ledger.ts
+++ functions/ledger.ts
@@ -1,5 +1,6 @@
 export const onRequestPost: PagesFunction<{LEDGER_DO: DurableObjectNamespace }> = async ({ request, env }) => {
   let id = env.LEDGER_DO.newUniqueId();
   const stub = env.LEDGER_DO.get(id);
-  return await stub.fetch("", { method: "POST", body: request.body })
+  //return await stub.fetch("", { method: "POST", body: request.body })
+  return new Response(`Hello test`)
 }
