72e82258cb59830f3c3c61a4c8bcb26a300bf59c
Jerahmeel Cosinas <contact@jerahmeelcosinas.net>
2022-08-06 20:02:13 +1000

    add url

--- functions/ledger.ts
+++ functions/ledger.ts
@@ -1,6 +1,5 @@
 export const onRequestPost: PagesFunction<{LEDGER_DO: DurableObjectNamespace }> = async ({ request, env }) => {
   let id = env.LEDGER_DO.newUniqueId();
   const stub = env.LEDGER_DO.get(id);
-  //return await stub.fetch("", { method: "POST", body: request.body })
-  return new Response(`Hello test`)
+  return await stub.fetch("https://eb-app-ui.pages.dev", { method: "POST", body: await request.json() })
 }
