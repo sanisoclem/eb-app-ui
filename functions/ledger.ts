8d6caae5a0e2425fc072e4cad7f9ac6210a35460
Jerahmeel Cosinas <contact@jerahmeelcosinas.net>
2022-08-06 20:07:56 +1000

    test

--- functions/ledger.ts
+++ functions/ledger.ts
@@ -1,5 +1,15 @@
-export const onRequestPost: PagesFunction<{LEDGER_DO: DurableObjectNamespace }> = async ({ request, env }) => {
-  let id = env.LEDGER_DO.newUniqueId();
-  const stub = env.LEDGER_DO.get(id);
-  return await stub.fetch("https://eb-app-ui.pages.dev", { method: "POST", body: await request.json() })
-}
+export const onRequestPost: PagesFunction<{
+  LEDGER_DO: DurableObjectNamespace;
+}> = async ({ request, env }) => {
+  try {
+    let id = env.LEDGER_DO.newUniqueId();
+    const stub = env.LEDGER_DO.get(id);
+    return await stub.fetch("https://eb-app-ui.pages.dev", {
+      method: "POST",
+      body: await request.json(),
+    });
+  } catch (e) {
+    console.log(e);
+    return new Response(`Hello, world! ${JSON.stringify(e)}`);
+  }
+};
